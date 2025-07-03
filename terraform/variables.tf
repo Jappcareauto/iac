f28258846e33724169219e2efa4c666d45f1dc87
Liquidator75 <akobtah77@gmail.com>
2025-07-03 20:19:50 +0100

    updated_ami

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -44,7 +44,7 @@ variable "iam-role" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-07041441b708acbd6" // Replace with the latest AMI ID for your region
+  default     = "ami-020cba7c55df1f615" // Replace with the latest AMI ID for your region
 }
 
 variable "instance_type" {
